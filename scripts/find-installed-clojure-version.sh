c3e9aa4276e66315fa0840d25c043fbe60ab034d
JarrodCTaylor <jarrod@fastmail.com>
2024-02-23 07:54:24 -0600

    Update env var name

--- scripts/find-installed-clojure-version.sh
+++ scripts/find-installed-clojure-version.sh
@@ -2,6 +2,6 @@
 
 # shellcheck disable=SC2164
 cd "$HOME/.m2/repository/org/clojure/clojure"
-LATEST_SNAPSHOT=$(find . -type d -name "*" -exec basename {} \; | sort -V | tail -n 1)
-echo "Latest snapshot version found: $LATEST_SNAPSHOT"
-echo "LATEST_SNAPSHOT=$LATEST_SNAPSHOT" >> "$GITHUB_ENV"
+LATEST_CLJ_BUILD=$(find . -type d -name "*" -exec basename {} \; | sort -V | tail -n 1)
+echo "Latest snapshot version found: $LATEST_CLJ_BUILD"
+echo "LATEST_CLJ_BUILD=$LATEST_CLJ_BUILD" >> "$GITHUB_ENV"
